fd80e2ab555f51205f687ff1c1cf5f283d5f3293
simon0302010 <100756138+simon0302010@users.noreply.github.com>
2025-09-05 21:21:23 +0200

    simple file choose ui

--- metaclean/core/gui.py
+++ metaclean/core/gui.py
@@ -1,12 +1,14 @@
 import sys
 
 from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtWidgets import QMainWindow, QPushButton, QLabel
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QLabel, QFileDialog, QVBoxLayout, QWidget
 
 
 class MetaClean(QMainWindow):
     def __init__(self):
         super().__init__()
+        
+        self.filenames = []
         
         self.setWindowTitle("MetaClean")
         self.setMinimumSize(QSize(400, 300))
@@ -14,6 +16,26 @@ class MetaClean(QMainWindow):
         self.show_start()
         
     def show_start(self):
-        button = QPushButton("Select Files")
+        label = QLabel("Select image files to remove their metadata.")
+        label.setAlignment(Qt.AlignCenter)
+
+        button = QPushButton("Select Images")
+        button.clicked.connect(self.open_file)
+
+        layout = QVBoxLayout()
+        layout.addStretch()
+        layout.addWidget(label)
+        layout.addWidget(button, alignment=Qt.AlignCenter)
+        layout.addStretch()
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        self.setCentralWidget(container)
         
-        self.setCentralWidget(button)
+    def open_file(self):
+        dlg = QFileDialog(self)
+        dlg.setFileMode(QFileDialog.ExistingFiles)
+        dlg.setNameFilter("Images (*.png *.jpg *.jpeg *.bmp *.gif)")
+        if dlg.exec_():
+            self.filenames = dlg.selectedFiles()
